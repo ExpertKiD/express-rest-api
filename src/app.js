24cdcf31a18bfec6bf67ec2cec124c2ebc539016
Suman Adhikari <expertxkid@gmail.com>
2021-02-22 17:39:13 +0545

    Add expresss to listen on host and port using http from .env file

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 // Load modules
 var dotenv = require('dotenv');
+var express = require('express');
 
+var app = express();
 
 // Load config from .env file
 dotenv.config();
+
+const PORT = process.env.APP_PORT;
+const HOST = process.env.APP_HOSTNAME; 
+
+app.listen(PORT, HOST, ()=>{
+    console.log(`Server running at http://${HOST}:${PORT}/`);
+});
