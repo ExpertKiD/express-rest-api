454be774a431a8fe7becb1a4a5722f854eb2e630
Suman Adhikari <expertxkid@gmail.com>
2021-02-22 18:31:30 +0545

    Add comments to app.js

--- src/app.js
+++ src/app.js
@@ -7,9 +7,11 @@ var app = express();
 // Load config from .env file
 dotenv.config();
 
+// set port and host
 const PORT = process.env.APP_PORT;
 const HOST = process.env.APP_HOSTNAME; 
 
+// listen on http(s)://host:port/
 app.listen(PORT, HOST, ()=>{
     console.log(`Server running at http://${HOST}:${PORT}/`);
 });
