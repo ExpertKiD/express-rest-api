3e2cda1793e5188536c0d2e40829ff4d72a5aa90
Suman Adhikari <expertxkid@gmail.com>
2021-06-20 15:15:16 +0545

    Check for Auth header in middleware

--- src/middlewares/authorize.js
+++ src/middlewares/authorize.js
@@ -1,5 +1,37 @@
-module.exports = function authorize(req, res, next){
+module.exports = async function authorize(req, res, next){
     console.log('TODO [2]: This module authorizes the user');
-    
+
+    let authHeader = req.headers.authorization;
+
+   if(authHeader === undefined){
+       res.setHeader('Content-Type','application/json');
+       res.status(400).end(JSON.stringify({
+           errors:[
+               "No Authorization header found."
+           ]
+       }));
+       return;
+   }
+
+    let [authMethod, authToken] = authHeader.split(' ');
+
+    switch(authMethod){
+        case null:
+        case '':
+            res.status(400).end({
+                errors: [
+                    "No Authorization header found."
+                ]
+            });
+            break;
+        case 'Bearer':
+
+            break;
+
+        default:
+
+            break;
+    }
+
     next();
 }
