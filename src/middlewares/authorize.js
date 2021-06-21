ab9446b1d692627d0dd05414dded0af2b47d8cbf
Suman Adhikari <expertxkid@gmail.com>
2021-06-21 10:21:34 +0545

    Add case for checking empty/undefined bearer token

--- src/middlewares/authorize.js
+++ src/middlewares/authorize.js
@@ -16,21 +16,29 @@ module.exports = async function authorize(req, res, next){
     let [authMethod, authToken] = authHeader.split(' ');
 
     switch(authMethod){
+
+        case 'Bearer':
+            if( authToken === undefined)
+            {
+                res.setHeader('Content-Type','application/json');
+                res.status(400).end(JSON.stringify({
+                    errors: [
+                        "No Bearer token found."
+                    ]
+                }));
+            }
+
+            break;
+
         case undefined:
         case '':
+        default:
             res.setHeader('Content-Type','application/json');
             res.status(400).end(JSON.stringify({
                 errors: [
                     "No Authorization header found."
                 ]
             }));
-            break;
-        case 'Bearer':
-
-            break;
-
-        default:
-
             break;
     }
 
